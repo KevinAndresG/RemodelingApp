643791a7aa98bfd28b7073ce7bba79d3752ad2be
KevinAndresG <kevinandresg@gmail.com>
2024-09-26 19:51:50 -0500

    Remodeling App Foundations

---
+++ src/Components/Contact/Contact.tsx
@@ -0,0 +1,73 @@
+function ContactSection() {
+  return (
+    <section
+      id="contacto"
+      className="py-10 px-6 bg-gradient-to-b from-gray-200 via-gray-100 to-white"
+    >
+      <div className="container mx-auto">
+        <h2 className="text-3xl font-bold text-center mb-12 text-cyan-500">
+          Contáctanos
+        </h2>
+        <div className="max-w-lg mx-auto">
+          <form className="space-y-2">
+            <div>
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium text-gray-700 mb-1"
+              >
+                Nombre
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700 mb-1"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="message"
+                className="block text-sm font-medium text-gray-700 mb-1"
+              >
+                Mensaje
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                rows={4}
+                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
+                required
+              ></textarea>
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-600 transition duration-300"
+              >
+                Enviar mensaje
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ContactSection;
